1a069788cb775d8f905c6af70821d7ddd1d5e217
Mrinmoy Mondal <mrinmoymondalreal@gmail.com>
2024-06-29 19:57:31 +0530

    added packages

--- backend/firebase/index.js
+++ backend/firebase/index.js
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDQxzZZjridV2dSvhYFCvgdPuClJpJ0D1k",
-  authDomain: "nutrilens-10137.firebaseapp.com",
-  projectId: "nutrilens-10137",
-  storageBucket: "nutrilens-10137.appspot.com",
-  messagingSenderId: "469717836043",
-  appId: "1:469717836043:web:ac6f2234dde132cbbec20e",
-  measurementId: "G-H3111PGSMW"
+  apiKey: process.env.EXPO_API_KEY,
+  authDomain: process.env.EXPO_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PROJECT_ID,
+  storageBucket: process.env.EXPO_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
